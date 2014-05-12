9c5fc0a67736eb5a301647a24f860debea36a279
SatoshiLiang <chihchengliang@gmail.com>
2014-05-12 15:51:14 +0800

    more lecture4 done

---
+++ l4-17_Value_Program.py
@@ -0,0 +1,77 @@
+# ----------
+# User Instructions:
+# 
+# Create a function compute_value() which returns
+# a grid of values. Value is defined as the minimum
+# number of moves required to get from a cell to the
+# goal. 
+#
+# If it is impossible to reach the goal from a cell
+# you should assign that cell a value of 99.
+
+# ----------
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+cost_step = 1 # the cost associated with moving from a cell to an adjacent one.
+
+# ----------------------------------------
+# insert code below
+# ----------------------------------------
+
+def compute_value():
+    closed = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[goal[0]][goal[1]] = 0
+
+    x = goal[0]
+    y = goal[1]
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            
+            if x == init[0] and y == init[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 99 and grid[x2][y2] == 0:
+                            g2 = g + cost_step
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = g2
+    
+    #for c in closed:
+    #    print c
+    return closed #make sure your function returns a grid of values as demonstrated in the previous video.
+
+
+print compute_value()
